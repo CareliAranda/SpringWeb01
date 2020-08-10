6a6a7bdd426dacfd5d9fc89a66aa83879997b289
maranda <maranda@queretaro.gob.mx>
2020-08-10 16:03:43 -0500

    Se agregan cambios del proyecto

---
+++ src/main/java/mx/gob/queretaro/rest/CityRest.java
@@ -0,0 +1,23 @@
+package mx.gob.queretaro.rest;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("ciudad")
+public class CityRest {
+
+	@GetMapping(path="saludo",produces = MediaType.APPLICATION_JSON_VALUE)
+	public String hello() {
+		return "Hola Mundo desde un Rest CARE";
+	}
+
+	@GetMapping("saludo2")
+	public String hello2() {
+		return "Hola Mundo 2";
+	}
+
+}
